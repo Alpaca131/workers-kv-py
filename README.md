c09de3ad5c94384b8cf2dfef31af1b0d9fa72996
Alpaca131 <iwa124816+github@gmail.com>
2022-04-02 16:06:30 +0000

    add setup.py and README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# What is this?
+This is the api wrapper of Cloudflare Workers KV.  
+You can use this to store and retrieve data from your Cloudflare Workers KV much easier.
+
+
+## Installation
+WIP
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='workers_kv.py',
+      version='1.0',
+      description='Api wrapper of Cloudflare Workers KV for Python',
+      author='Alpaca131',
+      author_email='contact@alpaca131.com',
+      url='https://github.com/Alpaca131/workers-kv-py',
+      install_requires=['requests']
+      )
