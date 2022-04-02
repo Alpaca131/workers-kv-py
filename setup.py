4dab2313e7bb885d75cddf8d5d02d6a7153f1581
Alpaca131 <iwa124816+github@gmail.com>
2022-04-02 16:16:48 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
-from distutils.core import setup
+import setuptools
 
-setup(name='workers_kv.py',
-      version='1.0',
-      description='Api wrapper of Cloudflare Workers KV for Python',
-      author='Alpaca131',
-      author_email='contact@alpaca131.com',
-      url='https://github.com/Alpaca131/workers-kv-py',
-      requires=['requests']
-      )
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="workers-kv.py",
+    version="1.0",
+    author="Alpaca131",
+    author_email="iwa124816@gmail.com",
+    description="An api wrapper of Cloudflare Workers KV for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Alpaca131/workers-kv-py",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests>=2.25.1"
+    ],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
